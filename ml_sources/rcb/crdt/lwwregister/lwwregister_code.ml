3bb00f678095fbf99ef037aff847bf25089010b9
Casper S <casperes1996@me.com>
2021-10-07 12:40:40 +0200

    Broken but progress lwwregister

--- ml_sources/rcb/crdt/lwwregister/lwwregister_code.ml
+++ ml_sources/rcb/crdt/lwwregister/lwwregister_code.ml
@@ -9,14 +9,13 @@ open Vector_clock_code
     Based on 
     https://hal.inria.fr/inria-00555588/document.
 *)
-type ('a, 'b) sumTy = InjL of 'a | InjR of 'b
 type registerVal = string (* Strings can sort of hold anything so for greatest flexibility a string is ued. Further serialisation/deserialisation can be done on the string to convert to any other data type. *)
 
 let op_ser = string_ser
 
 let op_deser = string_deser
 
-let eval (register:registerVal) = register
+let eval (register:registerVal) () = register
 
 let effect message (register:registerVal) our_ts our_id = 
     let ((newRegVal, their_vc), orig) = (fst (fst message)) in 
@@ -25,15 +24,16 @@ let effect message (register:registerVal) our_ts our_id =
       if their_ts = our_ts then (if orig <= our_id then newRegVal else register) (* <= is important since the msg may be from ourself* *)
       else register
 
-let prepare lock broadcast register value =
+let prepare lock broadcast (register:registerVal ref) value =
     acquire lock; 
     let message = broadcast value in
-    let ((_, our_vc), our_id) = message in 
+    let our_vc = snd (fst message) in
+    let our_id = snd (snd message) in 
     let our_ts = vect_nth our_vc our_id in 
     register := effect message !register our_ts our_id;
     release lock
 
-let apply_thread lock register deliver our_ts our_id =
+let apply_thread lock register our_ts our_id deliver =
   loop_forever (fun () ->
       acquire lock;
       begin
@@ -52,6 +52,6 @@ let register_init addrs rid =
     let broadcast = snd pair in
     let register = ref ("") in
     let lock = newlock () in 
-    fork (apply_thread lock set) deliver 0 rid;
-    (elems lock register, (size lock set, prepare lock broadcast register))
+    fork (apply_thread lock register 0 rid) deliver;
+    (eval !register, prepare lock broadcast register)
       
---
+++ ml_sources/rcb/crdt/mvregister/mvregister_runner.ml
@@ -0,0 +1,45 @@
+open Unix
+open Ast
+open Rcb_code
+open Serialization_code
+open Set_code
+open List_code
+open Lwwregister_code
+
+(** TODO: adapt the ml_sources/rcb/crdt/counter/counter_runner.ml file
+    to run and test the gset_code handle inputs and outputs *)
+let handle_io i elems size prepare = 
+    let readString = read_line () in 
+    match String.split_on_char ' ' readString with 
+    | [ "add"; value ] -> (
+        let () = prepare value in 
+        Printf.printf "Node[%d] Added: %s\n" i value
+    )
+    | [ "elems" ] -> (
+        Printf.printf "Node[%d] Set elements: " i;
+        set_iter (Printf.printf "%s,") (elems ());
+        Printf.printf "\n"
+    )
+    | [ "size" ] -> (
+        Printf.printf "Node[%d] Set size is: %d\n" i (size ())
+    )
+    | _ -> ()
+
+let init_exec () = 
+    if Array.length Sys.argv < 4 then (
+        prerr_endline "Usage: <index> <port1 port2 ... portN \n
+    to read the elements of the set type 'elems', to add something to the set type 'add <value>'. To read the size of the set, write size";
+        exit 2);
+      let ip = string_of_inet_addr (gethostbyname "localhost").h_addr_list.(0) in
+      let l =
+        let sa i = SADDR (ip, (int_of_string Sys.argv.(i + 2))) in
+        list_init (Array.length Sys.argv - 2) sa
+      in
+      let i = int_of_string Sys.argv.(1) in
+      let pair = set_init l i in 
+      let elems = fst pair in 
+      let size = fst (snd pair) in
+      let prepare = snd (snd pair) in
+      loop_forever (fun () -> handle_io i elems size prepare)
+
+let () = Unix.handle_unix_error init_exec ()
