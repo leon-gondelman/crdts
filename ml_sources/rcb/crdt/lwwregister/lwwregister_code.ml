196fa8eaea44b82d1b2f7700bfe27836ac2de170
Casper S <casperes1996@me.com>
2021-10-08 14:54:36 +0200

    lwwregister with relations

--- ml_sources/rcb/crdt/lwwregister/lwwregister_code.ml
+++ ml_sources/rcb/crdt/lwwregister/lwwregister_code.ml
@@ -4,54 +4,49 @@ open Serialization_code
 open List_code
 open Set_code
 open Vector_clock_code
+open Pure_op_based_framework
 
-(** Last Write Wins Register 
-    Based on 
-    https://hal.inria.fr/inria-00555588/document.
-*)
-type registerVal = string (* Strings can sort of hold anything so for greatest flexibility a string is ued. Further serialisation/deserialisation can be done on the string to convert to any other data type. *)
+(* Note messages are of the form: (((operation, value), vector clock), origin), register will only contain (((operation, value), vector clock), origin)  *)
 
-let op_ser = string_ser
+let op_ser = prod_ser string_ser string_ser 
 
-let op_deser = string_deser
+let op_deser = prod_deser string_deser string_deser
 
-let eval (register:registerVal ref) () = !register
-
-let effect message (register:registerVal) our_ts our_id = 
-    let ((newRegVal, their_vc), orig) = message in 
-    let their_ts = vect_nth their_vc orig in 
-    if their_ts > our_ts then newRegVal else
-      if their_ts = our_ts then (if orig <= our_id then newRegVal else register) (* <= is important since the msg may be from ourself* *)
-      else register
-
-let prepare lock broadcast (register:registerVal ref) value =
+let read lock register () = 
+  acquire lock;
+  let res = list_map (fun x -> snd (fst x)) !register in
+  release lock;
+  res
+  
+let effect message register = 
+  let rel = (fun m _ -> fst (fst m) = "clear") in 
+  let rel01 = (fun m1 m2 -> (vect_leq (snd m1) (snd m2))) || (vect_conc (snd m1) (snd m2) && snd (snd m1) >= snd (snd m2)) in (*Last conjunction uses correct message structure I think. Others need fix. *)
+  (* let stabilize = (fun x -> x) in *)
+  effectFW rel rel01 rel01 (fst message) register
+    
+let prepare lock broadcast register value =
     acquire lock; 
     let message = broadcast value in
-    let our_vc = snd (fst message) in
-    let our_id = snd message in 
-    let our_ts = vect_nth our_vc our_id in 
-    register := effect message !register our_ts our_id;
+    effect message register;
     release lock
 
-let apply_thread lock register our_ts our_id deliver =
+let apply_thread lock register deliver =
   loop_forever (fun () ->
       acquire lock;
       begin
         match (deliver ()) with
           Some message ->
-            register := effect message !register our_ts our_id;
-            (* Thread.delay 0.5; *)
+            effect message register;
         | None -> ()
       end;
       release lock;)
-
 
 let register_init addrs rid = 
     let pair = rcb_init op_ser op_deser addrs rid in 
     let deliver = fst pair in
     let broadcast = snd pair in
-    let register = ref ("") in
+    let register = ref (set_empty ()) in
     let lock = newlock () in 
-    fork (apply_thread lock register 0 rid) deliver;
-    (eval register, prepare lock broadcast register)
+    fork (apply_thread lock register) deliver;
+    (read lock register, prepare lock broadcast register)
       
