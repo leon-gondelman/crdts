b4e5da9fb8060faaccd941551eaf1e01491f39e9
Casper S <casperes1996@me.com>
2021-10-21 15:56:32 +0200

    finalised on new framework methodology

--- ml_sources/rcb/crdt/lwwregister/lwwregister_code.ml
+++ ml_sources/rcb/crdt/lwwregister/lwwregister_code.ml
@@ -2,57 +2,30 @@ open Ast
 open Rcb_stabilise
 open Serialization_code
 open List_code
+open Map_code
 open Set_code
 open Vector_clock_code
 open Pure_op_based_framework
 
-(* Note messages are of the form: (((operation, value), vector clock), origin), register will contain (((operation, value), vector clock), origin)  *)
+let rel (m1 : 'value msg) (s : 'value msg aset) =
+  match list_head s with
+  | Some m2 ->
+    (fst (fst (fst m1)) = "clear")
+    || ((vect_conc (snd (fst m1)) (snd (fst m2))) && (snd m1 >= snd m2))
+  | None -> false
 
-let op_ser = prod_ser string_ser string_ser 
+let rel01 (m1 : 'value msg) (m2 : 'value msg)  =
+  (vect_leq (snd (fst m1)) (snd (fst m2)))
+  || (vect_conc (snd (fst m1)) (snd (fst m2)) && snd m1 >= snd m2)
 
-let op_deser = prod_deser string_deser string_deser
+let known_queries: (string, string aset) known_queries =
+  map_insert
+    "read"
+    (fun pset -> list_map (snd) pset) (map_empty ())
 
-let read lock register () = 
-  acquire lock;
-  let res = list_map (fun x -> snd (fst (fst x))) !register in
-  release lock;
-  res
-  
-let effect message register = 
-  let rel = (fun m1 s -> match (list_head s) with 
-    | Some m2 -> (fst (fst (fst m1)) = "clear") 
-      || ((vect_conc (snd (fst m1)) (snd (fst m2))) && (snd m1 >= snd m2))
-    | None -> false 
-  )   
-  in 
-  let rel01 = (fun m1 m2 -> (vect_leq (snd (fst m1)) (snd (fst m2)))
-  || (vect_conc (snd (fst m1)) (snd (fst m2)) && snd m1 >= snd m2)) in
-  (* let stabilize = (fun x -> x) in *)
-  effectFW rel rel01 rel01 message register
-    
-let prepare lock broadcast register value =
-    acquire lock; 
-    let message = broadcast value in
-    effect message register;
-    release lock
+let stabilize _m s = s
 
-let apply_thread lock register deliver =
-  loop_forever (fun () ->
-      acquire lock;
-      begin
-        match (deliver ()) with
-          Some message ->
-            effect message register;
-        | None -> ()
-      end;
-      release lock;)
+let serializer = {s_ser = prod_ser string_ser string_ser; s_deser = prod_deser string_deser string_deser}
 
-let register_init addrs rid = 
-    let register = ref (set_empty ()) in
-    let pair = rcb_init op_ser op_deser addrs rid register (fun _ reg -> reg) in 
-    let deliver = fst pair in
-    let broadcast = snd pair in
-    let lock = newlock () in 
-    fork (apply_thread lock register) deliver;
-    (read lock register, prepare lock broadcast register)
-    
+let register_init addrs rid =
+  crdt_init addrs rid serializer ((rel, rel01), rel01) known_queries stabilize
--- ml_sources/rcb/crdt/mvregister/mvregister_code.ml
+++ ml_sources/rcb/crdt/mvregister/mvregister_code.ml
@@ -6,54 +6,6 @@ open Set_code
 open Vector_clock_code
 open Map_code
 open Pure_op_based_framework
-(*
-
-(* Note messages are of the form: (((operation, value), vector clock), origin), register contains (((operation, value), vector clock), origin)  *)
-
-let op_ser = prod_ser string_ser string_ser 
-
-let op_deser = prod_deser string_deser string_deser
-
-let read lock register () = 
-  acquire lock;
-  let res = list_map (fun x -> snd ( fst (fst x))) !register in
-  release lock;
-  res
-  
-let effect message register = 
-  let rel = (fun m _ -> fst (fst (fst m)) = "clear") in 
-  let rel01 = (fun m1 m2 -> vect_leq (snd (fst m1)) (snd (fst m2))) in 
-  (* let stabilize = (fun x -> x) in *)
-  effectFW rel rel01 rel01 (message) register
-    
-let prepare lock broadcast register value =
-    acquire lock; 
-    let message = broadcast value in
-    effect message register;
-    release lock
-
-let apply_thread lock register deliver =
-  loop_forever (fun () ->
-      acquire lock;
-      begin
-        match (deliver ()) with
-          Some message ->
-            effect message register;
-        | None -> ()
-      end;
-      release lock;)
-
-let register_init addrs rid = 
-    let register = ref (set_empty ()) in
-    let pair = rcb_init op_ser op_deser addrs rid register (fun _ reg -> reg) in 
-    let deliver = fst pair in
-    let broadcast = snd pair in
-    let lock = newlock () in 
-    fork (apply_thread lock register) deliver;
-    (read lock register, prepare lock broadcast register)
-      
-
-    *)
 
   let rel = (fun m _ -> fst (fst (fst m)) = "clear")
   let rel01 = (fun m1 m2 -> vect_leq (snd (fst m1)) (snd (fst m2)))
