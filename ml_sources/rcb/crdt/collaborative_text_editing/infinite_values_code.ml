22b9de65d5c05905df833473c5b08dfaa1fddb86
Casper S <casperes1996@me.com>
2021-11-01 17:38:32 +0100

    prefix works

--- ml_sources/rcb/crdt/collaborative_text_editing/infinite_values_code.ml
+++ ml_sources/rcb/crdt/collaborative_text_editing/infinite_values_code.ml
@@ -12,7 +12,7 @@ open Collaborative_editor_shared
 (* Note messages are of the form: (((operation, (position, value)), vector clock), origin), 
   the set contains whole messages *)
 
-let base = 1000000
+let base = 1_000_000
 
 let serializer = {s_ser = prod_ser string_ser (prod_ser (list_ser int_ser) string_ser); 
   s_deser = prod_deser string_deser (prod_deser (list_deser int_deser) string_deser)}
@@ -22,7 +22,7 @@ let prefix (list : int aset) (depth : int) =
   let listcpy = ref list in
   let copyBuilder = ref list_nil in 
   while !i <= depth do 
-    if !i < (list_length !listcpy) 
+    if !i <= (list_length !listcpy) 
     then (copyBuilder := (list_append !copyBuilder (list_cons (unSOME (list_head !listcpy)) None));
     listcpy := (list_tail !listcpy); 
     i := (!i+1))
