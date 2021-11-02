b2ee1808051d5f121a1113b91d14ee5e1bb5f4a1
Casper S <casperes1996@me.com>
2021-11-02 17:56:10 +0100

    Testing framework pretty

--- ml_sources/rcb/crdt/collaborative_text_editing/test_infinite.ml
+++ ml_sources/rcb/crdt/collaborative_text_editing/test_infinite.ml
@@ -1,8 +1,11 @@
 open Infinite_values_code
 open List_code
 
+let testsRun = ref 0
+let testsPass = ref 0
+
 let passTest check = 
-  if check then Printf.printf "\027[1;32mTEST SUCCESS\027[0m\n-----------------------\n\n" else Printf.printf "\027[1;31mTEST FAILED\027[0m\n-----------------------\n\n"
+  if check then Printf.printf "\027[1;32mTEST SUCCESS\027[0m\n-----------------------\n\n" else Printf.printf "\027[1;31mTEST FAILED\027[0m\n-----------------------\n\n"; check
 
 let test_and_print result expected = 
   let stringRes = ref "[" in 
@@ -11,7 +14,9 @@ let test_and_print result expected =
   Printf.printf "%s" !stringRes;
   Printf.printf "\n";
   Printf.printf "%s\n" expected;
-  passTest (!stringRes = expected)
+  let testSucc = passTest (!stringRes = expected) in 
+  (if testSucc then testsPass := (!testsPass+1));
+  testsRun := (!testsRun+1)
 
 
 let testList = list_cons 2_500 (list_cons 500_000 list_nil)
@@ -32,3 +37,10 @@ let testSubtractPositions =
     let testSubtractPositions3 = 
       Printf.printf "\nTestineg subtract_positions on list 3 and 1\n";
 test_and_print (subtract_positions testList3 testList) "[12000,0,0,]"
+
+
+    let () =
+      Printf.printf "All tests finished\n";
+      let color = if testsRun = testsPass then "\027[1;4;32m" else "\027[1;4;31m" in 
+      Printf.printf "%s%d/%d tests successfull\n" color !testsPass !testsRun;
+      if testsPass = testsRun then Printf.printf "All tests passed successfully!\027[0m\n" else Printf.printf "Some tests failed!\027[0m\n"
